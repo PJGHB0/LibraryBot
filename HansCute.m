0b296e4321546884cbfcb854a829af49372a8719
PJGHB0 <100071459+PJGHB0@users.noreply.github.com>
2022-04-20 13:17:37 +1000

    assignment info added

---
+++ HansCute.m
@@ -0,0 +1,25 @@
+classdef HansCute
+    %HansCuteRobot class
+    %   Robot class including the robot model, and all 
+    
+    properties 
+        %all of the variables
+
+    end
+    methods
+        %Examples below-----------------------
+        function obj = untitled(inputArg1,inputArg2)
+            %UNTITLED Construct an instance of this class
+            %   Detailed explanation goes here
+            obj.Property1 = inputArg1 + inputArg2;
+        end
+        function outputArg = method1(obj,inputArg)
+            %METHOD1 Summary of this method goes here
+            %   Detailed explanation goes here
+            outputArg = obj.Property1 + inputArg;
+        end
+        %-------------------------------------
+        function 
+    end
+end
+
