651fbc6391817a4d55f394d5fe7f9f9f696c9430
PJGHB0 <100071459+PJGHB0@users.noreply.github.com>
2022-05-16 21:35:21 +1000

    Update main.m

--- main.m
+++ main.m
@@ -12,4 +12,4 @@ myInterface.ReturnBook(3);
 % qMatrix = jtraj([0 0 0 0 0 0 0],myInterface.qtable_0_q1+myInterface.qShelf_Mid,50);
 % myInterface.MoveWithoutBook(qMatrix);
 % qMatrix = jtraj(myInterface.HansCute.qCurrent,[0 0 0 0 0 0 0],50);
-% myInterface.MoveWithBook(qMatrix,1);
+myInterface.MoveWithBook(qMatrix,1);
