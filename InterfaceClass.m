3da59a75e2110786907f2c03d982f90b021aadb9
PJGHB0 <100071459+PJGHB0@users.noreply.github.com>
2022-05-16 14:38:36 +1000

    Plan for main script updated
    Instead of having a simple script to combine everything we will do this in a class (so that functions can interact with environmnent objects). This should also make everything much cleaner

---
+++ InterfaceClass.m
@@ -0,0 +1,12 @@
+%% This is a class for our front end interface (pretty much does everything)
+classdef InterfaceClass < handle
+    properties
+        
+        
+    end
+    methods
+        function
+            
+        end
+    end
+end
